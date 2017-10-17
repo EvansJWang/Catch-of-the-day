e818bab75a01f8c1c885e059650f1cb384951d02
evans <evansjwang@gmail.com>
2017-10-17 17:27:11 -0400

    Add Load sample button and data

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,14 +2,37 @@ import React from 'react';
 import Header from './Header'
 import Order from './Order'
 import Inventory from './Inventory'
+import Fish from './Fish'
+import sampleFishes from '../sample-fishes'
 
 export default class App extends React.Component {
   constructor(props) {
     super(props)
+
+    this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
+
     this.state = {
       fishes: {},
       order: {}
     }
+  }
+
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    })
+  }
+
+  addFish(fish) {
+    // copy then update state
+    const fishes = {...this.state.fishes};
+    const timestamp = Date.now()
+    fishes[`fish-${timestamp}`] = fish
+    // set state
+    this.setState( {fishes} )
+    //same as this ^
+    //this.setState({ fishes: fishes })
   }
 
   render() {
@@ -17,9 +40,14 @@ export default class App extends React.Component {
       <div className='catch-of-the-day'>
         <div className='menu'>
           <Header tagline='Fresh Seafood Market'/>
+          <ul className="list-of-fish">
+            {
+              Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)
+            }
+          </ul>
         </div>
           <Order/>
-          <Inventory/>
+          <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     )
   }
--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -5,8 +5,9 @@ export default class Inventory extends React.Component {
   render() {
     return(
       <div>
-        <AddFishForm/>
-        <p> Inventory </p>
+        <h2> Inventory </h2>
+        <AddFishForm addFish={this.props.addFish}/>
+        <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </div>
     )
   }
